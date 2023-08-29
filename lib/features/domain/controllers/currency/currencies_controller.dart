328eb30338c1badcab511de05c9699f237183436
Vadim R <vahellame@gmail.com>
2023-08-29 20:51:52 +0300

    upd

--- lib/features/domain/controllers/currency/currencies_controller.dart
+++ lib/features/domain/controllers/currency/currencies_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import '../../models/currency_model/currency_model.dart';
 import '../../repositories/currencies_repository/local_currencies_repository.dart';
@@ -7,35 +8,36 @@ import '../controller.dart';
 import 'currencies_state.dart';
 
 class CurrenciesController extends Controller {
-  CurrenciesController({
-    required this.currenciesState,
-    required this.localCurrenciesRepository,
-    required this.remoteCurrenciesRepository,
-  });
+  CurrenciesController(
+      {required this.currenciesState, required this.localCurrenciesRepository, required this.remoteCurrenciesRepository,});
 
   final LocalCurrenciesRepository localCurrenciesRepository;
   final RemoteCurrenciesRepository remoteCurrenciesRepository;
   final CurrenciesState currenciesState;
 
-  List<CurrencyModel> get currencies  => currenciesState.currencies;
-  
+  List<CurrencyModel> get currencies => currenciesState.currencies;
+
   Future<void> init() async {
     loadingStarted();
     currenciesState.currencies = await localCurrenciesRepository.getAllCurrencies();
     loadingFinished();
     unawaited(_startFetchingCurrencies());
   }
-  
+
   Future<void> _startFetchingCurrencies() async {
     while (true) {
-      currenciesState.currencies = await remoteCurrenciesRepository.getAllCurrencies();
-      notifyListeners();
+      try {
+        currenciesState.currencies = await remoteCurrenciesRepository.getAllCurrencies();
+        notifyListeners();
 
-      for (final currency in currenciesState.currencies) {
-        await localCurrenciesRepository.upsertCurrency(currency);
+        for (final currency in currenciesState.currencies) {
+          await localCurrenciesRepository.upsertCurrency(currency);
+        }
+
+        await Future.delayed(const Duration(seconds: 5));
+      } catch (e, s) {
+        log('', name: 'ERROR', error: e, stackTrace: s);
       }
-
-      await Future.delayed(const Duration(seconds: 5));
     }
-  } 
+  }
 }
